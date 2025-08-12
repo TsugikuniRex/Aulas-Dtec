616da1a946b45b04c1d9ce481b7f3b2696bd53ef
TsugikuniRex <carloseduardooliveiragarrucho@gmail.com>
2025-08-12 08:38:40 -0300

    depois de beber

--- Aulas/Back/Aula 5 do 8/script.js
+++ Aulas/Back/Aula 5 do 8/script.js
@@ -7,6 +7,6 @@ numeroTitulo.innerHTML += numero;
 texto.innerHTML += `<h2><strong>A raiz quadrada do seu numero é: ${Math.sqrt(numero)}</strong></h2>`;
 texto.innerHTML += `<h2><strong>Seu inteiro é: ${Number.isInteger(numero)}</strong></h2>`;
 texto.innerHTML += `<h2><strong>Seu numero é: ${Number.isNaN(numero)}</strong></h2>`;
-texto.innerHTML += `<h2><strong>Seu numero arredondado pra baixo é: ${Math.floor(numero)}</strong></h2>`;
+texto.innerHTML += `<h2><strong>Seu numero arredondado pra baixo é: ${Math.floor(numero)}</strong></h2>`;   
 texto.innerHTML += `<h2><strong>Seu numero arredondado pra cima é: ${Math.ceil(numero)}</strong></h2>`;
 texto.innerHTML += `<h2><strong>Seu numero com dois zero é: ${numero.toFixed(2)}</strong></h2>`;
