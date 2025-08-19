b9691fe0d9e7e6be66b48ac261a24eebe2d262f6
TsugikuniRex <carloseduardooliveiragarrucho@gmail.com>
2025-08-19 11:27:39 -0300

    fala youtube

--- Projetos/projeto individual/assets/js/main.js
+++ Projetos/projeto individual/assets/js/main.js
@@ -9,24 +9,32 @@ e.preventDefault();
     const peso = form.querySelector('.peso').value
     const altura = form.querySelector('.altura').value
     
-    let IMC = peso / altura **2
-    IMC = Number(IMC.toFixed(2))
-    
-    if(IMC <= 18.5){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Abaixo do peso).</p>`
-    }else if(IMC >= 18.6 && IMC <= 24.9){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Peso normal).</p>`
-    }else if(IMC >= 25.0 && IMC <= 29.9){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Sobrepeso).</p>`
-    }else if(IMC >=30.0 && IMC <=34.9){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 1).</p>`
-    }else if(IMC >=35.0 && IMC <=39.9){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 2).</p>`
-    }else if(IMC >= 40){
-        resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 3).</p>`
-    }else{
-        resultado.innerHTML = `<p class = "bad">Valor Inválido</p>`
+    if(peso === "" || peso === undefined || peso ==0){
+        resultado.innerHTML = `<p class = "bad">Peso Inválido</p>`
+    }else if(altura === "" || altura === undefined || altura ==0){
+        resultado.innerHTML = `<p class = "bad">Altura Inválido</p>`
     }
-
+    else{
+        let IMC = peso / altura **2
+        IMC = Number(IMC.toFixed(2))
+    
+        if(IMC <= 18.5){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Abaixo do peso).</p>`
+        }else if(IMC >= 18.6 && IMC <= 24.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Peso normal).</p>`
+        }else if(IMC >= 25.0 && IMC <= 29.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Sobrepeso).</p>`
+        }else if(IMC >=30.0 && IMC <=34.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 1).</p>`
+        }else if(IMC >=35.0 && IMC <=39.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 2).</p>`
+        }else if(IMC >= 40){
+            resultado.innerHTML = `<p class="paragrafo-resultado">Seu peso IMC é ${IMC} (Obesidade de grau 3).</p>`
+        }else if(IMC/0){
+            resultado.innerHTML = `<p></p>`
+        }else{
+            resultado.innerHTML = `<p class = "bad">Valor Inválido</p>`
+        }
+    }
 })
 
