ea3f05319d3a44212fbd0d78ae3128ee136977b8
TsugikuniRex <carloseduardooliveiragarrucho@gmail.com>
2025-09-11 11:26:40 -0300

    atualização 7

--- Aulas/Back/setembro/API/server.js
+++ Aulas/Back/setembro/API/server.js
@@ -14,7 +14,8 @@ let usuarios = [
     {id: 1, nome: "Ana", idade:25},
     {id: 2, nome: "Carlos", idade:30},
     {id: 3, nome: "Maria", idade:22},
-    {id: 4, nome: "José Maria", idade:67}
+    {id: 4, nome: "José Maria", idade:67},
+    {id: 5, nome: "Marcos", idade:25}
 ]
 
 app.get("/", (req, res) =>{
@@ -49,9 +50,48 @@ app.get("/usuarios/buscar/:nome", (req,res) => {
         res.status(404).json({mensagem: "Usuário Não Encontrado"})
     }
 })
+app.get("/usuarios/idade/:idade", (req, res) => {
+    const buscaridade = req.params.idade
+    const usuario = usuarios.filter(u => u.idade == buscaridade)
+    if(usuario.length > 0){
+        res.json(usuario)
+    }else{
+        res.status(404).json({mensagem: "Usuario não Encontrado"})
+    }
+
+})
 
 
+app.delete("/usuarios/:id", (req, res) => {
+    const id = req.params.id
+    usuarios = usuarios.filter(u => u.id != id)
+    res.json({mensagem: "Usuario Removido com sucesso"})
+})
 
+app.post("/usuarios", (req, res) => {
+    const novoUsuario = {
+        id: usuarios.length +1,
+        nome: req.body.nome,
+        idade: req.body.idade
+    };
+
+    usuarios.push(novoUsuario)
+    res.status(201).json(novoUsuario)
+})
+
+app.put("/usuarios/:id", (req, res) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const idade = req.body.idade;
+
+    const usuario = usuarios.find(u => u.id == id)
+    if(!usuario){
+        return res.status(404).json({mensagem: "Usuario não Encontrado"})
+    }
+    usuario.nome = nome || usuario.nome;
+    usuario.idade = idade || usuario.idade;
+    res.json(usuario)
+})
 //iniciar o servidor
 app.listen(PORT, ()=>{
     console.log(`Servidor na porta ${PORT}`)
